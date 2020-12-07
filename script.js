b45f883734a8ea57e98a4cf960e4c18ca0a8c1c1
Mandi <haase1020@gmail.com>
2020-12-07 08:09:43 -0500

    complete score page

--- script.js
+++ script.js
@@ -41,6 +41,44 @@ let finalTimeDisplay = '0.0s';
 // Scroll
 let valueY = 0;
 
+// reset game
+function playAgain() {
+  gamePage.addEventListener('click', startTimer);
+  scorePage.hidden = true;
+  splashPage.hidden = false;
+  equationsArray = [];
+  playerGuessArray = [];
+  valueY = 0;
+  playAgainBtn.hidden = true;
+}
+
+
+//show score page
+function showScorePage() {
+  //show playAgain button after 1 sec
+  setTimeout(() => {
+    playAgainBtn.hidden = false;
+  }, 1000);
+  gamePage.hidden = true;
+  scorePage.hidden = false;
+}
+
+// format and display time in DOM
+function scoresToDOM() {
+finalTimeDisplay = finalTime.toFixed(1);
+baseTime = timePlayed.toFixed(1);
+penaltyTime = penaltyTime.toFixed(1);
+baseTimeEl.textContent = ` Base Time: ${baseTime}s`
+penaltyTimeEl.textContent = `Penalty Time: +${penaltyTime}s`;
+finalTimeEl.textContent = `${finalTimeDisplay}s`;
+//scroll to top, go to score page
+itemContainer.scrollTo({ top: 0, behavior: 'instant' });
+showScorePage();
+
+}
+
+
+
 // stop time, process results, go to score page
 function checkTime() {
   console.log(timePlayed)
@@ -58,6 +96,7 @@ function checkTime() {
     });
     finalTime = timePlayed + penaltyTime;
     console.log("time", timePlayed, "penalty: ", penaltyTime, "final", finalTime)
+    scoresToDOM();
   }
 }
 
